51f3573ed4f19432c7ad6d7504b70f228d8bf7fa
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-05-21 10:16:56 +0700

    update

--- resources/views/front-end/product.blade.php
+++ resources/views/front-end/product.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.frontend')
 @section('content')	
 
+<link rel="stylesheet" type="text/css" href="{{asset('pro-master/styles/services.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('pro-master/styles/services_responsive.css')}}">
+
 
 <div class="home d-flex flex-column align-items-start justify-content-end">
     <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="{{asset('image/slider/st.jpg')}}" data-speed="0.8"></div>
@@ -18,5 +21,73 @@
         </div>
     </div>
 </div>
+
+
+<div class="prices">
+    <div class="container">
+        <div class="row">
+
+            <!-- Price -->
+            <div class="col-lg-6 price_col">
+                <div class="price">
+                    <div class="price_title">Daily Protection Sunscreen</div>
+                    <div class="price_text">
+                        <p>Light textured daily sunscreen suitable for dry and oily skin types. Contains formula that gives extra protection against UVA & UVB SPF 50 PA+++</p>
+                    </div>
+                    <div class="price_panel">20ml</div>
+                </div>
+            </div>
+
+            <!-- Price -->
+            <div class="col-lg-6 price_col">
+                <div class="price">
+                    <div class="price_title">Night Radiance Cream</div>
+                    <div class="price_text">
+                        <p>Integer aliquet congue libero, eu gravida odio ultrces ut. Etiam ac erat ut enim maximus accumsan vel ac nisl.</p>
+                    </div>
+                    <div class="price_panel">20ml</div>
+                </div>
+            </div>
+
+            <!-- Price -->
+            <div class="col-lg-6 price_col">
+                <div class="price">
+                    <div class="price_title">Intense Brightening Serum</div>
+                    <div class="price_text">
+                        <p>Integer aliquet congue libero, eu gravida odio ultrces ut. Etiam ac erat ut enim maximus accumsan vel ac nisl.</p>
+                    </div>
+                    <div class="price_panel">20ml</div>
+                </div>
+            </div>
+
+            <!-- Price -->
+            <div class="col-lg-6 price_col">
+                <div class="price">
+                    <div class="price_title">Acne Care Serum</div>
+                    <div class="price_text">
+                        <p>Integer aliquet congue libero, eu gravida odio ultrces ut. Etiam ac erat ut enim maximus accumsan vel ac nisl.</p>
+                    </div>
+                    <div class="price_panel">20ml</div>
+                </div>
+            </div>
+
+            <!-- Price -->
+            <div class="col-lg-6 price_col">
+                <div class="price">
+                    <div class="price_title">Gentle Cleansing Gel</div>
+                    <div class="price_text">
+                        <p>Integer aliquet congue libero, eu gravida odio ultrces ut. Etiam ac erat ut enim maximus accumsan vel ac nisl.</p>
+                    </div>
+                    <div class="price_panel">120ml</div>
+                </div>
+            </div>
+
+            <!-- Price -->
+         
+
+        </div>
+    </div>
+</div>
+
 
 @endsection
