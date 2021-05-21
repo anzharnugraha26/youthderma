4ec5fe4509faea6fbb4cd2577e1709a6fa3b0eaf
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-05-21 09:47:44 +0700

    update

--- resources/views/front-end/doctor.blade.php
+++ resources/views/front-end/doctor.blade.php
@@ -21,5 +21,247 @@
 
 @include('front-end.include.doctor')
 
+<div class="intro">
+    <div class="container">
+        <div class="row">
+
+            <!-- Intro Content -->
+            <div class="col-lg-7">
+                <div class="intro_content">
+                    <div class="section_title_container">
+                        <div class="section_subtitle">This is Dr Pro</div>
+                        <div class="section_title"><h2 style="font-size: 35px">dr. Lia Permata Sari, dipl. CIBTAC</h2></div>
+                        <div class="team_title" style="font-size: 24px"> Head Doctor</div>
+                    </div>
+                    <div style="margin-top: 40px">
+                        MAIN INTEREST (S) 
+                        <p>Cosmetic,  Aesthetic & Anti Aging. Face Contouring, Botox, Dermal Filler Injection, Skin Booster, Threadlift.</p>
+                    </div>
+                    <div style="margin-top: 30px">
+                        ACHIEVEMENT (S)  
+                        <p>1. Restylane Dermal Filler Master Class Galderma 2019 </p>
+                        <p>2. IMCAS CONGRESS ACADEMY 2020  </p>
+                        <p>3. Neuvia Dermal Filler Face Contouring Workshop 2019 </p>
+                        <p>4. Professional Aesthetic Medical Jakarta, Esthetico Derma Institute 2018</p>
+                        <p>5. CIBTAC Diploma in Facial Applications 2018 UK.</p>
+                        <p>6. Participant BIOTEC Italia, Multi Laser to treat Different Case & True Body Liposclupture, Carla Aesthetic Institute 2018.</p>
+                    </div>
+                    <div style="margin-top: 30px">
+                        DOCTOR SCHEDULE 
+                        <p>Senin, Rabu, 10.00 – 16.00
+                            Kamis,
+                            10.00 – 19.00</p>
+                    </div>
+
+                    <!-- Milestones -->
+                    <div class="milestones">
+                        <div class="row milestones_row">
+                        
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="5000" data-sign-before="+">0</div>
+                                    <div class="milestone_text">Satisfied Patients</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="352">0</div>
+                                    <div class="milestone_text">Face Liftings</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="718">0</div>
+                                    <div class="milestone_text">Injectibles</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="5">0</div>
+                                    <div class="milestone_text">Awards Won</div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Intro Image -->
+            <div class="col-lg-4 offset-lg-1" style="margin-top: 100px">
+                <div class="team_image"><img src="{{asset('image/dr/dr-lia.jpg')}}" alt="" width="400px" height="460px"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="intro">
+    <div class="container">
+        <div class="row">
+
+            <!-- Intro Content -->
+            <div class="col-lg-7">
+                <div class="intro_content">
+                    <div class="section_title_container">
+                        <div class="section_subtitle">This is Dr Pro</div>
+                        <div class="section_title"><h2 style="font-size: 35px">dr. Anisa Amalia</h2></div>
+                        <div class="team_title" style="font-size: 24px">Aesthetic Doctor</div>
+                    </div>
+                    <div style="margin-top: 40px">
+                        MAIN INTEREST (S) 
+                        <p>Cosmetic,  Aesthetic & Anti Aging, Laser Treatment, Ultra Layer Lift, Botox Injection. </p>
+                    </div>
+                    <div style="margin-top: 30px">
+                        ACHIEVEMENT (S)  
+                        <p>1. Dermal Filler Face Contouring Injection Training Class 2020 </p>
+                        <p>2. Threadlift Double Fix Advanced Class 2021 </p>
+                        <p>3. Laser Treatment & Ultrasound Training Aesthetic Medicine </p>
+                    </div>
+                    <div style="margin-top: 30px">
+                        DOCTOR SCHEDULE 
+                        <p>Selasa, jumat, sabtu
+                            10.00-19.00 </p>
+                    </div>
+
+                    <!-- Milestones -->
+                    <div class="milestones">
+                        <div class="row milestones_row">
+                        
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="5000" data-sign-before="+">0</div>
+                                    <div class="milestone_text">Satisfied Patients</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="352">0</div>
+                                    <div class="milestone_text">Face Liftings</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="718">0</div>
+                                    <div class="milestone_text">Injectibles</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="5">0</div>
+                                    <div class="milestone_text">Awards Won</div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Intro Image -->
+            <div class="col-lg-4 offset-lg-1" style="margin-top: 100px">
+                <div class="team_image"><img src="{{asset('image/dr/dr-annisa.jpg')}}" alt="" width="400px" height="460px"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="intro">
+    <div class="container">
+        <div class="row">
+
+            <!-- Intro Content -->
+            <div class="col-lg-7">
+                <div class="intro_content">
+                    <div class="section_title_container">
+                        <div class="section_subtitle">This is Dr Pro</div>
+                        <div class="section_title"><h2 style="font-size: 35px">dr. Febrina Andarini,Sp.KK</h2></div>
+                        <div class="team_title" style="font-size: 24px">Dermatology and Venereology Specialist</div>
+                    </div>
+                    <div style="margin-top: 40px">
+                        MAIN INTEREST (S) 
+                        <p>Cosmetic Dermatology, Allergy Immunology and Sexually Transmitted Infection</p>
+                    </div>
+                    {{-- <div style="margin-top: 30px">
+                        ACHIEVEMENT (S)  
+                        <p>1. Dermal Filler Face Contouring Injection Training Class 2020 </p>
+                        <p>2. Threadlift Double Fix Advanced Class 2021 </p>
+                        <p>3. Laser Treatment & Ultrasound Training Aesthetic Medicine </p>
+                    </div> --}}
+                    <div style="margin-top: 30px">
+                        DOCTOR SCHEDULE 
+                        <p>Senin, Rabu
+                            16.00- 19.00 </p>
+                    </div>
+
+                    <!-- Milestones -->
+                    <div class="milestones">
+                        <div class="row milestones_row">
+                        
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="5000" data-sign-before="+">0</div>
+                                    <div class="milestone_text">Satisfied Patients</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="352">0</div>
+                                    <div class="milestone_text">Face Liftings</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="718">0</div>
+                                    <div class="milestone_text">Injectibles</div>
+                                </div>
+                            </div>
+
+                            <!-- Milestone -->
+                            <div class="col-md-3 milestone_col">
+                                <div class="milestone">
+                                    <div class="milestone_counter" data-end-value="5">0</div>
+                                    <div class="milestone_text">Awards Won</div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Intro Image -->
+            <div class="col-lg-4 offset-lg-1" style="margin-top: 100px">
+                <div class="team_image"><img src="{{asset('image/dr/dr-febrina.jpg')}}" alt="" width="400px" height="460px"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
