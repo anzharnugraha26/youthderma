fd9d5948dbdf10716c3f53cc019523542f6e9783
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-05-27 11:29:03 +0700

    update

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -39,28 +39,6 @@ class GoogleController extends Controller
             return redirect('/')->with('with','aa');
         }
         }
-        // try{
-        //     $user = Socialite::driver('google')->user();
-        //     // dd($user);
-        //     $finduser = User::where('google_id', $user->getId())->first();
-        //     if ($finduser) {
-        //         Auth::login($finduser);
-        //         return redirect('/');
-        //     }
-        //     else{
-        //         $newuser = User::create([
-        //             'name' => $user->name,
-        //             'email' => $user->email,
-        //             'google_id' => $user->id,
-        //             'password' => bcrypt('${1:my-secret-password')
-        //         ]);
-        //         Auth::login($newuser);
-        //         return redirect('/');
-        //     }
-          
-        //     } catch(\Throwable $th){
-
-        //     }
     }
 
 
