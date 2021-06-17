a27ee31f1cf35a69ee137d69c1dc049008fda2eb
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-06-17 15:15:25 +0700

    update

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -6,15 +6,15 @@
         <div class="home-slideshow">
             <div class="slide">
                 <div class="blur-up lazyload">
-                    <img class="blur-up lazyload" data-src="{{asset('image/shop-slider/home.jpg')}}" src="{{asset('image/shop-slider/home.jpg')}}" alt="Shop Our New Collection" title="Shop Our New Collection" />
+                    <img class="blur-up lazyload" data-src="{{asset('image/shop-slider/1.jpg')}}" src="{{asset('image/shop-slider/home.jpg')}}" alt="Shop Our New Collection" title="Shop Our New Collection" />
                     <div class="slideshow__text-wrap slideshow__overlay classic middle">
                         <div class="slideshow__text-content middle">
                             <div class="container">
-                                <div class="wrap-caption right">
+                                {{-- <div class="wrap-caption right">
                                     <h2 class="h1 mega-title slideshow__title">Our New Collection</h2>
                                     <span class="mega-subtitle slideshow__subtitle">Save up to 50% Off</span>
                                     <span class="btn">Shop now</span>
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -22,15 +22,47 @@
             </div>
             <div class="slide">
                 <div class="blur-up lazyload">
-                    <img class="blur-up lazyload" data-src="{{asset('image/shop-slider/main_home.jpg')}}" src="{{asset('image/shop-slider/main_home.jpg')}}" alt="Summer Bikini Collection" title="Summer Bikini Collection" />
+                    <img class="blur-up lazyload" data-src="{{asset('image/shop-slider/2.jpg')}}" src="{{asset('image/shop-slider/main_home.jpg')}}" alt="Summer Bikini Collection" title="Summer Bikini Collection" />
                     <div class="slideshow__text-wrap slideshow__overlay classic middle">
                         <div class="slideshow__text-content middle">
                             <div class="container">
-                                <div class="wrap-caption center">
-                                    {{-- <h2 class="h1 mega-title slideshow__title">Fashion &amp; Show</h2>
-                                    <span class="mega-subtitle slideshow__subtitle">A World Fashion and Trendy Fashion Clother's</span> --}}
+                                {{-- <div class="wrap-caption center">
+                                    <h2 class="h1 mega-title slideshow__title">Fashion &amp; Show</h2>
+                                    <span class="mega-subtitle slideshow__subtitle">A World Fashion and Trendy Fashion Clother's</span>
                                     <span class="btn">Shop now</span>
-                                </div>
+                                </div> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slide">
+                <div class="blur-up lazyload">
+                    <img class="blur-up lazyload" data-src="{{asset('image/shop-slider/3.jpg')}}" src="{{asset('image/shop-slider/main_home.jpg')}}" alt="Summer Bikini Collection" title="Summer Bikini Collection" />
+                    <div class="slideshow__text-wrap slideshow__overlay classic middle">
+                        <div class="slideshow__text-content middle">
+                            <div class="container">
+                                {{-- <div class="wrap-caption center">
+                                    <h2 class="h1 mega-title slideshow__title">Fashion &amp; Show</h2>
+                                    <span class="mega-subtitle slideshow__subtitle">A World Fashion and Trendy Fashion Clother's</span>
+                                    <span class="btn">Shop now</span>
+                                </div> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slide">
+                <div class="blur-up lazyload">
+                    <img class="blur-up lazyload" data-src="{{asset('image/shop-slider/4.jpg')}}" src="{{asset('image/shop-slider/main_home.jpg')}}" alt="Summer Bikini Collection" title="Summer Bikini Collection" />
+                    <div class="slideshow__text-wrap slideshow__overlay classic middle">
+                        <div class="slideshow__text-content middle">
+                            <div class="container">
+                                {{-- <div class="wrap-caption center">
+                                    <h2 class="h1 mega-title slideshow__title">Fashion &amp; Show</h2>
+                                    <span class="mega-subtitle slideshow__subtitle">A World Fashion and Trendy Fashion Clother's</span>
+                                    <span class="btn">Shop now</span>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
