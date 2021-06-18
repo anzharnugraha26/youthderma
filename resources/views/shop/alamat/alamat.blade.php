94f1b10c2c37db95c813429b541278b13f40fe9e
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-06-18 13:22:17 +0700

    update

--- resources/views/shop/alamat/alamat.blade.php
+++ resources/views/shop/alamat/alamat.blade.php
@@ -5,7 +5,7 @@
     <!--Page Title-->
     <div class="page section-header text-center">
         <div class="page-title">
-            <div class="wrapper"><h1 class="page-width">Create an Account</h1></div>
+            <div class="wrapper"><h1 class="page-width">Add Address</h1></div>
           </div>
     </div>
     <!--End Page Title-->
