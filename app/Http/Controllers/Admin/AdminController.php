eb7ac1906b84b1d880769b73ba8e3682bd0ba4d1
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-06-22 09:39:22 +0700

    update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,13 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Booking;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     
     public function index()
     {
-        return view('admin.index');
+        $pendapatan = DB::table('orders')
+        ->select(DB::raw('SUM(subtotal) as penghasilan'))
+        ->where('status_order_id',5)
+        ->first();
+        $order_baru =  DB::table('orders')
+        ->join('status_order','status_order.id','=','orders.status_order_id')
+        ->join('users','users.id','=','orders.user_id')
+        ->select('orders.*','status_order.name','users.name as nama_pemesan')
+        
+        ->get();
+        return view('admin.index' , compact('pendapatan','order_baru'));
     }
 
    
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -29,7 +29,83 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        
+        <div class="row">
+          <div class="col-lg-4 col-6">
+            <div class="small-box bg-info">
+              <div class="inner">
+                <?php $d= DB::table('orders')->where('status_order_id', 1)->count(); ?>
+                <h3><?php echo $d;?></h3>
+
+                <p>New Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="{{url('admin/transaksi')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>{{ 'Rp.' . number_format($pendapatan->penghasilan) }}</h3>
+
+                <p>Income</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="{{url('admin/transaksi-selesai')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <?php $e= DB::table('users')->count(); ?>
+                <h3><?php echo $e;?></h3>
+                <p>Customers</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 grid-margin">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">10 Transaksi Terbaru</h4>
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th> Invoice </th>
+                        <th> Pemesan </th>
+                        <th> Subtotal </th>
+                        <th> Status Pesanan </th>
+                        <th> Aksi </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach($order_baru as $order)
+                        <tr>
+                          <td>{{ $order->invoice }}</td>
+                          <td>{{ $order->nama_pemesan }}</td>
+                          <td>{{ $order->subtotal }}</td>
+                          <td>{{ $order->name }}</td>
+                          <td> <a href="{{url("admin/transaksi-detail/$order->id")}}" class="btn btn-warning btn-sm">Detail</a></td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     
